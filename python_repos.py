ca4fab7c1768ddbe0530f1f0130ff5d28ec798e5
Mikhail <Misha9404@yandex.ru>
2023-08-18 18:05:38 +0300

    python_repos.py continues

--- python_repos.py
+++ python_repos.py
@@ -8,6 +8,19 @@ print(f"Status code: {r.status_code}")
 
 # Сохранение ответа API в переменной.
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
-# Обработка результатов.
-print(response_dict.keys())
+# Анализ результатов о репозиториях.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Анализ первого репозитория.
+repo_dict = repo_dicts[0]
+print(f"\nSelected information about first repository:")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
