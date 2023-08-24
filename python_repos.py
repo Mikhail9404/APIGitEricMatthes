ca627ed9fc6ded8819847c9cb3098ceca5c037d3
Mikhail <Misha9404@yandex.ru>
2023-08-24 19:15:22 +0300

    Visualization data - python_repos_visual.py

--- python_repos.py
+++ python_repos.py
@@ -16,11 +16,10 @@ print(f"Repositories returned: {len(repo_dicts)}")
 
 # Анализ первого репозитория.
 repo_dict = repo_dicts[0]
-print(f"\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+print(f"\nSelected information about each repository:\n")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}\n")
